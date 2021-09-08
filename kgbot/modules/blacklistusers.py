342e0e1eaf6dcda2aa419787ad6d350ccc7b071b
kalolonte1 <89563449+kalolonte1@users.noreply.github.com>
2021-09-08 22:11:15 +0700

    Update blacklistusers.py

--- kgbot/modules/blacklistusers.py
+++ kgbot/modules/blacklistusers.py
@@ -1,7 +1,7 @@
 # Module to blacklist users and prevent them from using commands by @TheRealPhoenix
 import html
-import innexiaBot.modules.sql.blacklistusers_sql as sql
-from innexiaBot import (
+import kgbot.modules.sql.blacklistusers_sql as sql
+from kgbot import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -10,12 +10,12 @@ from innexiaBot import (
     WOLVES,
     dispatcher,
 )
-from innexiaBot.modules.helper_funcs.chat_status import dev_plus
-from innexiaBot.modules.helper_funcs.extraction import (
+from kgbot.modules.helper_funcs.chat_status import dev_plus
+from kgbot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from innexiaBot.modules.log_channel import gloggable
+from kgbot.modules.log_channel import gloggable
 from telegram import ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
