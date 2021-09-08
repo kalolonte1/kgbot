060657d3484781c5ba26f31d0b391c06469759c8
kalolonte1 <89563449+kalolonte1@users.noreply.github.com>
2021-09-08 22:50:07 +0700

    Update welcome.py

--- kgbot/modules/welcome.py
+++ kgbot/modules/welcome.py
@@ -5,8 +5,8 @@ import time
 from functools import partial
 from contextlib import suppress
 
-import innexiaBot.modules.sql.welcome_sql as sql
-import innexiaBot
+import kgbot.modules.sql.welcome_sql as sql
+import kgbot
 from kgbot import (
     DEV_USERS,
     LOGGER,
@@ -265,7 +265,7 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome yourself
             elif new_mem.id == bot.id:
                 creator = None
-                if not innexiaBot.ALLOW_CHATS:
+                if not kgbot.ALLOW_CHATS:
                     with suppress(BadRequest):
                          update.effective_message.reply_text(f"Groups are disabled for {bot.first_name}, I'm outta here.")
                     bot.leave_chat(update.effective_chat.id)
