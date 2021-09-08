8110c4dd65cd4b331c7159eb3e192103073e8098
kalolonte1 <89563449+kalolonte1@users.noreply.github.com>
2021-09-08 22:13:31 +0700

    Update logomaker.py

--- kgbot/modules/logomaker.py
+++ kgbot/modules/logomaker.py
@@ -84,13 +84,13 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./innexiaBot/resources/blackbg.jpg')
+    img = Image.open('./kgbot/resources/blackbg.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./innexiaBot/resources/Chopsic.otf", 330)
+    font = ImageFont.truetype("./kgbot/resources/Chopsic.otf", 330)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -124,13 +124,13 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./innexiaBot/resources/blackbg.jpg')
+    img = Image.open('./kgbot/resources/blackbg.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./innexiaBot/resources/Maghrib.ttf", 1000)
+    font = ImageFont.truetype("./kgbot/resources/Maghrib.ttf", 1000)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -140,7 +140,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
     fname2 = "LogoByInnexia.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @innexiaBot")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @kgbot")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
